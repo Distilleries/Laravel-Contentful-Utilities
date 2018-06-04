0f85a7823e5983d28a368e19e360b91e19fdbfe3
Sébastien Muller <smuller@big-boss-studio.com>
2018-06-04 15:57:03 +0200

    Setup Locales related

--- src/Distilleries/Contentful/Commands/Sync/Locales.php
+++ src/Distilleries/Contentful/Commands/Sync/Locales.php
@@ -3,6 +3,9 @@
 namespace Distilleries\Contentful\Commands\Sync;
 
 use Illuminate\Console\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use Distilleries\Contentful\Models\Locale;
+use Distilleries\Contentful\Api\Management\Api;
 
 class Locales extends Command
 {
@@ -17,12 +20,48 @@ class Locales extends Command
     protected $description = 'Refresh Contentful space locales data';
 
     /**
+     * Contentful Management API.
+     *
+     * @var \Distilleries\Contentful\Api\Management\Api
+     */
+    protected $api;
+
+    /**
+     * Locales command constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->api = new Api;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
-        $this->line('Sync CF locales');
+        Locale::query()->truncate();
+
+        try {
+            $data = $this->api->locales();
+        } catch (GuzzleException $e) {
+            $this->error($e->getMessage());
+            return;
+        }
+
+        foreach ($data['items'] as $locale) {
+            Locale::query()->create([
+                'name' => $locale['name'],
+                'code' => $locale['code'],
+                'fallback_code' => $locale['fallbackCode'],
+                'is_editable' => ! empty($locale['contentManagementApi']),
+                'is_publishable' => ! empty($locale['contentDeliveryApi']),
+            ]);
+        }
     }
 }
--- src/Distilleries/Contentful/Models/Locale.php
+++ src/Distilleries/Contentful/Models/Locale.php
@@ -2,7 +2,41 @@
 
 namespace Distilleries\Contentful\Models;
 
-class Locale
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $code
+ * @property string $fallback_code
+ * @property boolean $is_editable
+ * @property boolean $is_publishable
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+class Locale extends Model
 {
-    //
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'locales';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = [
+        'name',
+        'code',
+        'fallback_code',
+        'is_editable',
+        'is_publishable',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $casts = [
+        'is_editable' => 'boolean',
+        'is_publishable' => 'boolean',
+    ];
 }
