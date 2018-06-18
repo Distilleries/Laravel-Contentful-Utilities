47ec2b78b2dbc53567247138ba7a478512b24ebd
Sébastien Muller <smuller@big-boss-studio.com>
2018-06-19 01:19:06 +0200

    Create generic Caster helper

---
+++ src/Distilleries/Contentful/Helpers/Caster.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Distilleries\Contentful\Helpers;
+
+use Parsedown;
+
+class Caster
+{
+    /**
+     * Cast value to a string.
+     *
+     * @param  mixed  $str
+     * @return string
+     */
+    public static function string($str) : string
+    {
+        return (string) $str;
+    }
+
+    /**
+     * Transform data to its JSON representation.
+     *
+     * @param  mixed  $data
+     * @return string
+     */
+    public static function json($data) : string
+    {
+        return ! empty($data) ? json_encode($data) : '';
+    }
+
+    /**
+     * Transform markdown content to an HTML string.
+     *
+     * @param  mixed  $md
+     * @return string
+     */
+    public static function markdown($md) : string
+    {
+        if (empty($md)) {
+            return '';
+        }
+
+        return (new Parsedown)->setBreaksEnabled(true)->text($md);
+    }
+
+    /**
+     * Cast an integer to an integer value otherwise to null.
+     *
+     * @param  mixed  $int
+     * @return integer|null
+     */
+    public static function integer($int) : ?int
+    {
+        return is_numeric($int) ? (int) $int : null;
+    }
+}
