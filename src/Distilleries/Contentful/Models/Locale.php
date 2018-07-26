ea54d93b2b09b2897ff17861b923c649e2771f2c
mfrancois <mfrancois@big-boss-studio.com>
2018-07-26 18:34:27 +0200

    Fix first query

--- src/Distilleries/Contentful/Models/Locale.php
+++ src/Distilleries/Contentful/Models/Locale.php
@@ -218,7 +218,7 @@ class Locale extends Model
             ->where('locale', $locale)
             ->take(1)
             ->get()
-            ->firs();
+            ->first();
 
         return empty($localeModel) ? static::default() : $localeModel->locale;
     }
