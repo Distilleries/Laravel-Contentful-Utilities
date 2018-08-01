beae1d05c6c142988ff6979bc76e4568b19c0992
mfrancois <mfrancois@big-boss-studio.com>
2018-08-01 14:40:00 +0200

    Add fill on model

--- src/Distilleries/Contentful/Models/Location.php
+++ src/Distilleries/Contentful/Models/Location.php
@@ -17,4 +17,31 @@ class Location
         'lat'
     ];
 
+    /**
+     * @param  array $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->fill($attributes);
+    }
+
+    /**
+     * Fill the model with an array of attributes.
+     *
+     * @param  array $attributes
+     * @return $this
+     *
+     */
+    public function fill(array $attributes)
+    {
+        foreach ($attributes as $key => $value) {
+            if (in_array($key, $this->fillable)) {
+                $this->{$key} = $value;
+            }
+        }
+
+        return $this;
+    }
+
 }
