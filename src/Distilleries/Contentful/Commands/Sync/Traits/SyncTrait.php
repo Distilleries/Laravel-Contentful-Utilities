487230ad23d5d9a915c10462039ed2a684ee093d
mfrancois <mfrancois@big-boss-studio.com>
2018-08-02 15:42:31 +0200

    Fix sync trait

--- src/Distilleries/Contentful/Commands/Sync/Traits/SyncTrait.php
+++ src/Distilleries/Contentful/Commands/Sync/Traits/SyncTrait.php
@@ -21,20 +21,20 @@ trait SyncTrait
     /**
      * Dump `sync` database into a SQL file and return its path.
      *
-     * @param  boolean  $isPreview
+     * @param  boolean $isPreview
      * @return string
      */
-    protected function dumpSync(bool $isPreview, string $connector='mysql'): string
+    protected function dumpSync(bool $isPreview, string $connector = 'mysql'): string
     {
         $path = storage_path('dumps/' . date('YmdHis') . '_sync' . ($isPreview ? '_preview' : '') . '.sql');
         $this->warn('Dump "' . basename($path) . '"...');
 
         $dirName = dirname($path);
-        if (! is_dir($dirName)) {
+        if (!is_dir($dirName)) {
             mkdir($dirName, 0777, true);
         }
 
-        $this->dumpSql($path, $connector);
+        $this->dumpSql($path, $this->getConnector($isPreview, $connector));
 
         return realpath($path);
     }
@@ -42,8 +42,8 @@ trait SyncTrait
     /**
      * Dump SQL database in given file path.
      *
-     * @param  string  $path
-     * @param  string  $connector
+     * @param  string $path
+     * @param  string $connector
      * @return void
      */
     protected function dumpSql(string $path, string $connector)
@@ -51,7 +51,7 @@ trait SyncTrait
         $exec = 'export MYSQL_PWD=\'%s\'; mysqldump --add-drop-table --default-character-set=%s %s -u %s -h %s --port %s > %s';
 
         $command = sprintf($exec,
-            addcslashes(config('database.connections.' . $connector . '.password'),"'"),
+            addcslashes(config('database.connections.' . $connector . '.password'), "'"),
             config('database.connections.' . $connector . '.charset'),
             config('database.connections.' . $connector . '.database'),
             config('database.connections.' . $connector . '.username'),
@@ -62,28 +62,42 @@ trait SyncTrait
         exec($command);
     }
 
+    protected function getConnector(bool $isPreview, string $connector = 'mysql'): string
+    {
+        $compiledConnector = $connector . ($isPreview ? '_preview' : '');
+        if (empty(config('database.connections.' . $compiledConnector . '.username'))) {
+            $compiledConnector = $connector;
+        }
+
+        return $compiledConnector;
+
+    }
+
+
     /**
      * Put previous dump in live-preview database.
      *
-     * @param  string  $path
+     * @param  string $path
      * @param  boolean $isPreview
      * @return void
      */
-    protected function putSync(string $path, bool $isPreview, string $connector='mysql')
+    protected function putSync(string $path, bool $isPreview, string $connector = 'mysql')
     {
+        $compiledConnector = $this->getConnector($isPreview, $connector);
+
         config([
-            'database.default' => 'mysql' . ($isPreview ? '_preview' : ''),
+            'database.default' => $compiledConnector,
         ]);
 
-        $this->warn('Put into "' . $connector . '" database...');
-        $this->putSql($path, $connector);
+        $this->warn('Put into "' . $compiledConnector . '" database...');
+        $this->putSql($path, $compiledConnector);
     }
 
     /**
      * Put SQL file into given database.
      *
-     * @param  string  $path
-     * @param  string  $connector
+     * @param  string $path
+     * @param  string $connector
      * @return void
      */
     protected function putSql(string $path, string $connector)
@@ -91,7 +105,7 @@ trait SyncTrait
         $exec = 'export MYSQL_PWD=\'%s\'; mysql -u %s -h %s --port %s %s < %s';
 
         $command = sprintf($exec,
-            addcslashes(config('database.connections.' . $connector . '.password'),"'"),
+            addcslashes(config('database.connections.' . $connector . '.password'), "'"),
             config('database.connections.' . $connector . '.username'),
             config('database.connections.' . $connector . '.host'),
             config('database.connections.' . $connector . '.port'),
