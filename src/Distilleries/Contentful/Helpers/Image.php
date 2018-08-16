5eb1c09127dd2460b3c1bcd438b994bec73f717a
mfrancois <mfrancois@big-boss-studio.com>
2018-08-16 16:57:33 +0200

    Explode  url method to make less responsible

--- src/Distilleries/Contentful/Helpers/Image.php
+++ src/Distilleries/Contentful/Helpers/Image.php
@@ -7,17 +7,24 @@ class Image
     /**
      * Return image URL to serve based on given parameters.
      *
-     * @param  string  $url
-     * @param  integer  $width
-     * @param  integer  $height
-     * @param  string  $format
-     * @param  integer  $quality
-     * @param  boolean|null  $useProgressive
-     * @param  string  $fit
+     * @param  string $url
+     * @param  integer $width
+     * @param  integer $height
+     * @param  string $format
+     * @param  integer $quality
+     * @param  boolean|null $useProgressive
+     * @param  string $fit
      * @return string
      */
-    public static function url(string $url, int $width = 0, int $height = 0, $format = '', int $quality = 0, ?bool $useProgressive = null, $fit = ''): string
-    {
+    public static function url(
+        string $url,
+        int $width = 0,
+        int $height = 0,
+        $format = '',
+        int $quality = 0,
+        ?bool $useProgressive = null,
+        $fit = ''
+    ): string {
         if (empty($url)) {
             return '';
         }
@@ -25,32 +32,30 @@ class Image
         $imageUrl = '';
 
         $format = static::detectFormat($format);
-        if ($format === 'webp') {
-            $useProgressive = false;
-        }
-        if ($useProgressive === null) {
-            $useProgressive = ! empty(config('contentful.image.use_progressive'));
-        }
-
-        if (empty($fit)) {
-            $fit = 'fill';
-        } else {
-            $fit = ($fit !== 'default') ? $fit : null;
-        }
 
         collect([
             'w' => $width,
             'h' => $height,
-            'q' => ! empty($quality) ? $quality : config('contentful.image.default_quality'),
+            'q' => !empty($quality) ? $quality : config('contentful.image.default_quality'),
             'fm' => config('contentful.image.use_webp') ? $format : null,
-            'fl' => $useProgressive ? 'progressive' : null,
-            'fit' => $fit,
+            'fl' => static::detectProgressive($format, $useProgressive) ? 'progressive' : null,
+            'fit' => static::detectFit($fit),
         ])->filter(function ($value) {
-            return ! empty($value);
+            return !empty($value);
         })->each(function ($value, $key) use (& $imageUrl) {
             $imageUrl .= $key . '=' . $value . '&';
         });
 
+        return static::replaceHosts($url, $imageUrl);
+    }
+
+    /**
+     * @param null|string $url
+     * @param string $imageUrl
+     * @return string
+     */
+    protected static function replaceHosts(?string $url, string $imageUrl): string
+    {
         if (str_contains($url, '?')) {
             $url = explode('?', $url);
             $url = $url[0];
@@ -58,17 +63,18 @@ class Image
 
         $searchHosts = config('contentful.image.search_hosts');
         $replaceHost = config('contentful.image.replace_host');
-        if (! empty($searchHosts) && ! empty($replaceHost)) {
+        if (!empty($searchHosts) && !empty($replaceHost)) {
             $url = str_replace(explode(',', $searchHosts), $replaceHost, $url);
         }
 
-        return ! empty($url) ? $url . '?' . trim($imageUrl, '&'): '';
+        return !empty($url) ? $url . '?' . trim($imageUrl, '&') : '';
+
     }
 
     /**
      * Auto-detect image format to serve (based on browser capability).
      *
-     * @param  string  $format
+     * @param  string $format
      * @return string|null
      */
     protected static function detectFormat(string $format = ''): ?string
@@ -78,11 +84,45 @@ class Image
 
         if (empty($format)) {
             $browser = mb_strtolower($agent->browser());
-            if (($browser === 'chrome') && ! $agent->isMobile()) {
+            if (($browser === 'chrome') && !$agent->isMobile()) {
                 $format = config('contentful.image.use_webp') ? 'webp' : null;
             }
         }
 
         return $format;
+    }
+
+    /**
+     * @param null|string $fit
+     * @return null|string
+     */
+    protected static function detectFit(?string $fit = null): ?string
+    {
+
+        if (empty($fit)) {
+            $fit = 'fill';
+        } else {
+            $fit = ($fit !== 'default') ? $fit : null;
+        }
+
+        return $fit;
+    }
+
+    /**
+     * @param null|string $format
+     * @param bool|null $useProgressive
+     * @return bool
+     */
+    protected static function detectProgressive(?string $format = '', ?bool $useProgressive = null): bool
+    {
+        if ($format === 'webp') {
+            $useProgressive = false;
+        }
+
+        if ($useProgressive === null) {
+            $useProgressive = !empty(config('contentful.image.use_progressive'));
+        }
+
+        return $useProgressive;
     }
 }
