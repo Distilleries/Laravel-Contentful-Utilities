38d62dafbf5c9de7a996062f2951d2132b046c7c
mfrancois <mfrancois@big-boss-studio.com>
2018-09-24 16:50:43 +0200

    :sparkles: Introducing new features. Add relation type on contentful model entries to have only the one we need

--- src/database/migrations/2019_09_24_110626_add_relation_from_entry_relationships_table.php
+++ src/database/migrations/2019_09_24_110626_add_relation_from_entry_relationships_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRelationFromSyncEntryRelationshipsTable extends Migration
+class AddRelationFromEntryRelationshipsTable extends Migration
 {
     /**
      * Run the migrations.
