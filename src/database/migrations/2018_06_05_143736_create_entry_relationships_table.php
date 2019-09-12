d300df72b565031a8686c7a58d961b75db1cfb87
Sébastien Muller <smuller@big-boss-studio.com>
2019-09-12 15:08:14 +0200

    :construction: Comment too long migrations indexes keys

--- src/database/migrations/2018_06_05_143736_create_entry_relationships_table.php
+++ src/database/migrations/2018_06_05_143736_create_entry_relationships_table.php
@@ -22,13 +22,17 @@ class CreateEntryRelationshipsTable extends Migration
             $table->string('related_contentful_type')->index();
             $table->integer('order')->unsigned()->default(0);
 
-            $table->index(['locale', 'source_contentful_id'], 'locale_source_contentful_id_index');
-            $table->index(['country','locale', 'source_contentful_id'], 'cl_source_contentful_id_index');
-            $table->index(['locale', 'source_contentful_id', 'related_contentful_id', 'related_contentful_type'], 'locale_source_related_index');
-            $table->index(['country','locale', 'source_contentful_id', 'related_contentful_id', 'related_contentful_type'], 'cl_source_related_index');
-            $table->index(['locale', 'related_contentful_id'], 'locale_related_contentful_id_index');
-            $table->index(['country','locale', 'related_contentful_id'], 'cl_related_contentful_id_index');
-            $table->index(['country','locale', 'related_contentful_id', 'source_contentful_id', 'source_contentful_type'], 'cl_related_source_index');
+            $table->index(['locale', 'source_contentful_id'], 'l_sid_idx');
+            $table->index(['country', 'locale', 'source_contentful_id'], 'c_l_sid_idx');
+            // @TODO... Specified key was too long
+            // $table->index(['locale', 'source_contentful_id', 'related_contentful_id', 'related_contentful_type'], 'l_sid_rid_rty_idx');
+            // @TODO... Specified key was too long
+            // $table->index(['country', 'locale', 'source_contentful_id', 'related_contentful_id', 'related_contentful_type'], 'c_l_sid_rid_rty_idx');
+            $table->index(['locale', 'related_contentful_id'], 'l_rid_idx');
+            $table->index(['country', 'locale', 'related_contentful_id'], 'c_l_rid_idx');
+            // @TODO... Specified key was too long
+            // $table->index(['country', 'locale', 'related_contentful_id', 'source_contentful_id', 'source_contentful_type'], 'c_l_rid_sid_sty_idx');
+
         });
     }
 
